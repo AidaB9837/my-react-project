8c0f0a8530bb5f873d83b40357e8d12ab0b05513
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-05-27 01:21:20 +0200

    feat: Components-08

--- src/App.js
+++ src/App.js
@@ -9,3 +9,6 @@ export class App extends React.Component {
                 </div>        
     }
 }
+/*Components-08
+Can you use the `Hello` component more than once in the `App` component? What happens if you do?
+Yes, I can. If I do, I will see the `Hello` component as many time as I have inserted it into the `App` component.*/
