9d52947e7006895e919a92b2ca189838b11a2073
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-05-27 00:48:18 +0200

    feat: Components-03

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { HelloWorld } from "./HelloWorld";
 
-
+//Components-03: Write a class component called App that renders the Hello component within a div tag.
 export class App extends React.Component {
     render() {
-        return <h1> <HelloWorld /> </h1>        
+        return <div>
+                    <h1> <HelloWorld /> </h1>
+                </div>        
     }
 }
