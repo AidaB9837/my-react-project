3ba59b4c098e0d3acf8c24c77719d874c32c9e00
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-05-27 00:51:23 +0200

    feat: Components-04

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
 import { App } from './App'
 
+//Components-04: Create a new `index.js` file within the `src` folder and render the `App` component using the `ReactDOM.render` method.
 const root = document.querySelector('#root')
 
 ReactDOM.render(<App />, root)
