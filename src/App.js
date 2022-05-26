c0c27ffc27be9432de44d9ceb6b99322c45d614b
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-05-27 00:42:21 +0200

    feat: Components-02

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { HelloWorld } from "./HelloWorld";
+
+
+export class App extends React.Component {
+    render() {
+        return <h1> <HelloWorld /> </h1>        
+    }
+}
---
+++ src/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//Components-02: Write a `Hello` class components that renders the `"Hello, World!"` message within an `h1` tag.
+export class HelloWorld extends React.Component {
+    render() {
+        return <h1>Hello, World!</h1>
+    }
+}
