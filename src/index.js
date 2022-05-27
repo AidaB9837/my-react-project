6dd9ae53fe696eeb70ee6536e261a264bd5f1476
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-05-27 21:04:03 +0200

    feat: modified Components-04 from ReactDOM to createRoot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
+import { StrictMode } from "react"
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
 //Components-04: Create a new `index.js` file within the `src` folder and render the `App` component using the `ReactDOM.render` method.
+/*
 const root = document.querySelector('#root')
 
 ReactDOM.render(<App />, root)
+*/
+//But, ReactDOM has been deprecated, than I will use createRoot:
+
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement)
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
