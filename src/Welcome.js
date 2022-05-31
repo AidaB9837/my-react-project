f9341b6fa27a9edcf36d2c6c375dbbd4dbe2cb26
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-05-31 20:43:17 +0200

    feat: Props-04

--- src/Welcome.js
+++ src/Welcome.js
@@ -16,7 +16,7 @@ export class Welcome extends React.Component {
 
 /*Props-02: What happens if no `name` prop is passed to the `Welcome` component?
 Can you set a default value for the `name` prop?
-If no name` prop is passed to the `Welcome` component, only the part of the string that I have compiled will be rendered, that is "Welcome,".
+If no `name` prop is passed to the `Welcome` component, only the part of the string that I have compiled will be rendered, that is "Welcome,".
 Yes, I can set a default value for the `name` prop.*/
 Welcome.defaultProps = {
     name:"Aida"
@@ -26,4 +26,4 @@ Welcome.defaultProps = {
 within a `p` tg after the message `"Your age is"`. SVOLTO ALLA RIGA 10*/
 
 /*Props-04: What happens if the `name` prop is a JSX expression instead of a string? How do you pass a prop that contains a JSX expression?
-Modify the value passed to the `name` prop so that it's contained within a `strong` tag. */
+Modify the value passed to the `name` prop so that it's contained within a `strong` tag. SVOLTO ALLA RIGA 10*/
