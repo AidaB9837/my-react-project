bcfcce1bc9b2398a5b38fb68a01e221e0a7c07ee
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-06-06 18:47:10 +0200

    feat: Conditional Rendering-05

--- src/Age.js
+++ src/Age.js
@@ -4,6 +4,12 @@ import React from "react";
 Pass to the `Age` component the `age` prop that `Welcome` is receiving from the `App` component. */
 export class Age extends React.Component {
     render() {
-        return <p>Your age is {this.props.age}</p>
+        return (
+            <>
+                {this.props.age > 18 ? <p>Your age is {this.props.age}</p> : <p>You are very young!</p>}
+            </>
+        )
     }
 }
+
+//Conditional Rendering-05: Modify the `Age` component so that the `"Your age is "` message is rendered only if the `age` prop is greater than `18`. Otherwise render the `"You are very young!"` message. SVOLTO ALLA RIGA 9.
