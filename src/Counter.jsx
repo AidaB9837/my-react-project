dee50d881a7ec8ac604aeb4f577d4def598d62f7
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-06-17 20:06:22 +0200

    feat: Component Lifecycle-01

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -5,19 +5,18 @@ import { CounterDisplay } from "./CounterDisplay"
 The `Counter` component should render the `count` property within an `h1` tag, and the `count` property should be incremented by `1` every second;*/
 
 export class Counter extends React.Component{
+// Start Mounting Phase
     state = {
-        count : this.props.initialValue
+        count: this.props.initialValue
     }
 
-    constructor(props) {
-        super(props)
-
-        setInterval(() => {
-            this.setState({
-                count : this.state.count + this.props.incrementAmount
-            })
-        }, this.props.incrementInterval)
+    componentDidMount() {
+        this._interval = setInterval(() => 
+        this.setState({
+            count : this.state.count + this.props.incrementAmount
+        }), this.props.incrementInterval)
     }
+
     render() {
         return <>
                     <CounterDisplay count={this.state.count}/>
@@ -39,3 +38,6 @@ Why?*/
 
  /*State 05:Reset the counter to the initial value received as a prop when the value is greater than ten times
  that initial value.*/
+
+ /*Component Lifecycle 01: Modify the `Counter` component so that the interval is initialized 
+ within the `componentDidMount` life cycle method instead of the constructor. Is the constructor still required? No*/
