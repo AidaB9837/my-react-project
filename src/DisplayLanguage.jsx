155e32979cbde6b0f6db5aa0a916163cef5541e2
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-06-25 16:02:21 +0200

    feat: Context-02

--- src/DisplayLanguage.jsx
+++ src/DisplayLanguage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { LanguageContext } from "./LanguageContext";
+
+/*Context 02: Consume the `LanguageContext` within the `DisplayLanguage` component by using the context consumer, 
+and show the selected language in an `h1` tag.*/
+const String = {
+    en: {
+        currentLanguage: "You have selected the English language!"
+    },
+    
+    it: {
+        currentLanguage: "Hai selezionato la lingua Italiana!"
+    }
+}
 
 export class DisplayLanguage extends React.Component {
     render() {
-        return
+        return <LanguageContext.Consumer>
+            {language => {
+                return <h1>{String[language].currentLanguage}</h1>
+            }}
+        </LanguageContext.Consumer>
     }
 }
