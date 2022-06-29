d285493db5dd89907ab8b4e0e54b9b7b076e1eaa
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-06-29 17:21:40 +0200

    update useEffect-04

--- src/GithubUserList.jsx
+++ src/GithubUserList.jsx
@@ -2,8 +2,6 @@
 showing a GithubUser component for each username entered. 
 The usernames should be added to the array using an input field and a button. */
 
-//DA RIVEDERE INSIEME A JACOPO!!
-
 import { useState } from "react"
 import { GithubUser } from "./GithubUser"
 
@@ -18,16 +16,18 @@ export function GithubUserList() {
     function handleAddUsername(){
         setUsernames((usernames) => usernames.concat(newUser))
     }
-
+    console.log(usernames)
     return <>
+                <h1>Github Users List:</h1>
                 <ul>
                     {usernames.map((name, index) =>
                     <li key={name + index}>
-                        <GithubUser />
+                        <GithubUser username={name}/>
                     </li>)}
                 </ul>
                 <input
                 type="text"
+                placeholder="Write here Github User"
                 value={newUser}
                 onChange={handleInputUsername}></input>
                 <button onClick={handleAddUsername}>Add New User</button>
