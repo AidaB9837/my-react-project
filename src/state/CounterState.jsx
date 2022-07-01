b455c5651277f6ef99a1de1d29137d8ccd9b1ad6
Aìda Brugnone <aidabrugnone9@gmail.com>
2022-07-01 20:01:34 +0200

    feat: Redux-01

---
+++ src/state/CounterState.jsx
@@ -0,0 +1,48 @@
+/*Redux 01: Create a CounterReducer, along with its action types and action creators, to keep track of a counter 
+that can be incremented, decremented and reset. */
+
+const defaultState = 0
+
+const INCREMENT = "COUNTER@INCREMENT"
+const DECREMENT = "COUNTER@DECREMENT"
+const RESET = "COUNTER@RESET"
+
+export function incrementCounter(by = 1) {
+    return {
+        type: INCREMENT,
+        payload: by
+    }
+}
+
+export function decrementCounter(by = 1) {
+    return {
+        type: DECREMENT,
+        payload: by
+    }
+}
+
+export function reset() {
+    return {
+        type: RESET
+    }
+}
+
+export function counterReducer(state = defaultState, action) {
+    switch(action.type) {
+        case INCREMENT: {
+            return state + action.payload
+        }
+
+        case DECREMENT: {
+            return state - action.payload
+        }
+
+        case RESET: {
+            return defaultState
+        }
+
+        default: {
+            return state
+        }
+    }
+}
